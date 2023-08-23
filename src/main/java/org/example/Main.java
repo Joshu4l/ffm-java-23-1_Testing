df57f98bed7b27593f94986d9db83e72553ccc43
joshuaalbert <albertjoshua5@gmail.com>
2023-08-23 16:07:55 +0200

    added 5 methods as well as their corresponding tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,44 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Starting Point");
+        System.out.println("--------------");
     }
+
+
+    //Eine Methode, die die Summe von zwei ganzen Zahlen berechnet
+    public static int sum(int number1, int number2) {
+
+        return number1 + number2;
+    }
+
+
+    //Eine Methode, die prüft, ob eine gegebene Zahl gerade ist (even)
+    public static boolean isEven(int inputNumber) {
+
+        return inputNumber % 2 == 0;
+    }
+
+
+    //Eine Methode, die das Produkt zweier ganzer Zahlen berechnet.
+    public static int multiply(int number1, int number2){
+
+        return number1 * number2;
+    }
+
+
+    //Eine Methode, die eine gegebene Zeichenkette in Großbuchstaben umwandelt.
+    public static String turnIntoCapitalLetters(String inputString) {
+
+        return inputString.toUpperCase();
+    }
+
+
+    //Eine Methode, die prüft, ob eine gegebene Zahl positiv ist.
+    public static boolean isPositive(int inputNumber) {
+
+        return inputNumber >= 0;
+    }
+
+
 }
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,85 @@
+//Necessary Imports
+package org.example;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class MainTest {
+
+    //Schreibe einen Test für eine Methode, die die Summe von zwei ganzen Zahlen berechnet.
+    @Test
+    public void calculate_sum_given_two_integers() {
+        //GIVEN
+        int number1 = 3;
+        int number2 = 5;
+
+        //WHEN
+        int actual = Main.sum(number1, number2);
+        int expected = 8;
+
+        //THEN
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    //Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl gerade ist (even).
+    @Test
+    public void state_true_given_even_number() {
+        //GIVEN
+        int number = 2;
+
+        //WHEN
+        boolean actual = Main.isEven(number);
+        boolean expected = true;
+
+        //THEN
+        Assertions.assertEquals(expected,actual);
+    }
+
+
+    //Schreibe einen Test für eine Methode, die das Produkt zweier ganzer Zahlen berechnet.
+    @Test
+    public void calculate_product_given_two_integers() {
+        //GIVEN
+        int number1 = 3;
+        int number2 = 5;
+
+        //WHEN
+        int actual = Main.multiply(number1, number2);
+        int expected = 15;
+
+        //THEN
+        Assertions.assertEquals(expected,actual);
+    }
+
+
+    //Schreibe einen Test für eine Methode, die eine gegebene Zeichenkette in Großbuchstaben umwandelt.
+    @Test
+    public void state_capitalized_string_given_any_string() {
+        //GIVEN
+        String inputString = "abcdefg";
+
+        //WHEN
+        String actual = Main.turnIntoCapitalLetters(inputString);
+        String expected = "ABCDEFG";
+
+        //THEN
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    //Schreibe einen Test für eine Methode, die prüft, ob eine gegebene Zahl positiv ist.
+    @Test
+    public void state_true_given_positive_number() {
+        //GIVEN
+        int number = 18;
+
+        //WHEN
+        boolean actual = Main.isPositive(number);
+        boolean expected = true;
+
+        //THEN
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+}
